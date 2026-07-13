76359efdbb77307abc2f4c1ebb4fabbc64b2f136
Zhuravlevaes <e.s.juravleva@gmail.com>
2026-07-13 22:24:00 +0300

    Initial PWA version

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='greek-b1-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./data.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
